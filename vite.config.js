52acfb1881694478871b162c0655af4508de7e46
Jhon A. Romero Gonzaga <romerogonzaga21@gmail.com>
2023-10-05 03:41:35 -0500

    ci: add deploy with GPH

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,8 @@ import { viteStaticCopy } from "vite-plugin-static-copy";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/erlangb/",
+  public: "public",
   plugins: [
     react(),
     viteStaticCopy({
