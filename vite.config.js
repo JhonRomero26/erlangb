58c45ada170282fdaf55c60352a9233acaee80da
Jhon A. Romero Gonzaga <romerogonzaga21@gmail.com>
2023-10-05 12:01:43 -0500

    chore: refactor vite config for dev and prod

--- vite.config.js
+++ vite.config.js
@@ -3,23 +3,35 @@ import react from "@vitejs/plugin-react";
 import topLevelAwait from "vite-plugin-top-level-await";
 import assemblyScriptPlugin from "vite-plugin-assemblyscript-asc";
 
+const commonPlugins = [
+  react(),
+  topLevelAwait({
+    promiseExportName: "__tla",
+    promiseImportName: (i) => `__tla_${i}`,
+  }),
+  assemblyScriptPlugin({
+    projectRoot: ".",
+    srcEntryFile: "assembly/index.ts",
+    configFile: "asconfig.json",
+    srcMatch: "assembly",
+    targetWasmFile: "assembly/build/assembly.wasm",
+    distFolder: "dist",
+  }),
+];
+
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: "/erlangb/",
-  public: "public",
-  plugins: [
-    react(),
-    assemblyScriptPlugin({
-      projectRoot: ".",
-      srcEntryFile: "assembly/index.ts",
-      configFile: "asconfig.json",
-      srcMatch: "assembly",
-      targetWasmFile: "assembly/build/assembly.wasm",
-      distFolder: "dist",
-    }),
-    topLevelAwait({
-      promiseExportName: "__tla",
-      promiseImportName: (i) => `__tla_${i}`,
-    }),
-  ],
+export default defineConfig(({ mode }) => {
+  if (mode === "development") {
+    return {
+      base: "/",
+      public: "public",
+      plugins: commonPlugins,
+    };
+  }
+
+  return {
+    base: "/erlangb/",
+    public: "public",
+    plugins: commonPlugins,
+  };
 });
